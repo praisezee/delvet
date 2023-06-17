f86964f9f3779a1cf3689b94e551ab4ae7d05ab2
Apus Industries <folorunsopraise580@gmail.com>
2023-06-17 02:10:22 +0100

    admin page almost done

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 
-const DEV_URL = 'http://localhost:3500'
+//const DEV_URL = 'http://localhost:3500'
+const BASE_URL ='https://delvet.onrender.com'
 export default axios.create( {
-      baseURL: DEV_URL
+      baseURL: BASE_URL
 } );
 
 export const axiosPrivate = axios.create( {
-      baseURL: DEV_URL,
+      baseURL: BASE_URL,
       headers: {"Content-Type": "application/json"},
       withCredentials: true
 })
