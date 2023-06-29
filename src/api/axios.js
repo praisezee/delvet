14ac59eea360e7f8b6bb9159ed72d023f157d9bc
Folorunso praise Oluwapelumi <106927972+praisezee@users.noreply.github.com>
2023-06-29 06:51:56 +0100

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 //const DEV_URL = 'http://localhost:3500'
-const BASE_URL ='server.delvet.com.ng'
+const BASE_URL ='https://server.delvet.com.ng'
 export default axios.create( {
       baseURL: BASE_URL
 } );
